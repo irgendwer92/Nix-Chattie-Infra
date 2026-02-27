9d2b6f49966dce4d759083d3c237eeedf5f9fb28
irgendwer92 <55090043+irgendwer92@users.noreply.github.com>
2026-02-27 15:16:17 +0100

    Add sops-nix based secret management scaffold

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,7 +17,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, disko, ... }:
+  outputs = { self, nixpkgs, sops-nix, home-manager, disko, ... }:
     let
       system = "x86_64-linux";
       lib = nixpkgs.lib;
@@ -31,7 +35,7 @@
         lib.nixosSystem {
           inherit system;
           specialArgs = {
-            inherit self disko;
+            inherit self disko sops-nix;
           };
           modules =
             commonModules
